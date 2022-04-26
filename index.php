b3922d941e01e017f925cce674e1dc1be8646919
ANAYA Romain <anayaromain66@gmail.com>
2022-04-26 12:04:30 +0200

    instalation de composer pour l'autoload

--- index.php
+++ index.php
@@ -5,13 +5,6 @@ use App\App;
 const DS = DIRECTORY_SEPARATOR;
 const PATH_ROOT = __DIR__ . DS;
 
-
-function loadClasses($class)
-{
-
-    require_once './' . str_replace('\\', '/', $class) . '.php';
-}
-
-spl_autoload_register('loadClasses');
+require_once PATH_ROOT . 'vendor/autoload.php';
 
 App::startApp()->start();
